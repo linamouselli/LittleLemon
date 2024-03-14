0681a4c972bd51c8501df1ca6b6e84ed804d2b98
Lolo <linamoslly96@gmail.com>
2024-03-14 05:38:49 +0300

    add_tests

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -7,8 +7,8 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [ 
     #path('', views.sayHello, name='sayHello'), 
     path('', views.index, name='index'),
-    path('menu/', views.MenuItemsView.as_view()),
-    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('menu-items/', views.MenuItemsView.as_view(), name='menu'),
+    path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
     path('message/', views.msg),
     path('api-token-auth/', obtain_auth_token),
     
